344b5624691844ef2fdff9b1e8fc41497cb43d71
roong0 <github@woolemi.com>
2023-12-13 12:58:48 +1100

    q1.14 ex1 complete

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -12,17 +12,35 @@ const App = () => {
     'The only way to go fast, is to go well.'
   ]
    
-  const [selected, setSelected] = useState(0)
+  const [selected, setSelected] = useState(0);
+  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0));
+  const [mostVotes, setMostVotes] =useState(0);
 
   const handleClick = () => {
-    console.log(anecdotes);
     setSelected(Math.floor(Math.random()*(anecdotes.length-1)));
+  }
+  const handleVote = () => {
+    let temp = votes;
+    temp[selected] = temp[selected] + 1;
+    console.log(temp);
+      setVotes(temp);// Note that the displayed votes wont change here as it will only update once force to
+    // Updates the most votes
+    for (let i = 0;i < votes.length; i++) {
+      if (votes[i] > votes[mostVotes]) setMostVotes(i);
+    }
+    console.log(mostVotes);
   }
 
   return (
     <div>
-      {anecdotes[selected]}
+      <h1>Anecdote of the day</h1>
+      <p>{anecdotes[selected]}</p>
+      <p>has {votes[selected]} votes</p>
+      <button onClick={handleVote}>vote</button>
       <button onClick={handleClick}>next anecdote</button>
+      <h1>Anecdote with most votes</h1>
+      <p>{anecdotes[mostVotes]}</p>
+      <p>has {votes[mostVotes]} votes</p>
     </div>
   )
 }
