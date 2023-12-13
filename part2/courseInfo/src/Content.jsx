b03dcfc9bbc840ffb6b4be72ce06cd20c054379e
roong0 <github@woolemi.com>
2023-12-13 20:34:04 +1100

    Q2.2 & 2.3

--- part2/courseInfo/src/Content.jsx
+++ part2/courseInfo/src/Content.jsx
@@ -1,11 +1,13 @@
 import Part from "./Part"
 
 const Content = (props) => {
+      const total = props.course.parts.reduce((s, p) => s+p.exercises,0);// Doesnt work unless has an initial value.
     return (
       <div>
         {props.course.parts.map(part => 
           <Part text={part.name} value={part.exercises} key={part.id}/>
         )}
+        <b>total of {total} exercises</b>
       </div>
     )
   }
