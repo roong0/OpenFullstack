c911f95e3bd5e11995d19abf48d654be3cc4ee5c
roong0 <github@woolemi.com>
2023-12-14 12:17:42 +1100

    Q2.7

--- part2/phoneBook/src/App.jsx
+++ part2/phoneBook/src/App.jsx
@@ -3,9 +3,10 @@ import Numbers from './Numbers'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number:'0123456' }
   ]) 
-  const [newName, setNewName] = useState('')
+  const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
 
 
   const addNote = (event) => {
@@ -13,14 +14,18 @@ const App = () => {
     if (persons.filter(e=>e.name === newName).length > 0) {
       alert(`${newName} is already added to phonebook`);
     } else {
-      setPersons(persons.concat({name:newName}));
-    }
-    
+      // Doesnt check for valid phonenumber input
+      setPersons(persons.concat({name:newName, number:newNumber}));
+    } 
   }
 
+
   const handleNoteChange = (event) => {
-    console.log(event.target.value)
     setNewName(event.target.value)
+  }
+  const handleNumberChange = (event) => {
+    console.log(event.target.value);
+    setNewNumber(event.target.value);
   }
    
   return (
@@ -29,6 +34,9 @@ const App = () => {
       <form onSubmit={addNote}>
         <div>
           name: <input onChange={handleNoteChange}/>
+        </div>
+        <div>
+          number: <input onChange={handleNumberChange}/>
         </div>
         <div>
           <button type="submit" onClick={addNote}>add</button>
@@ -41,6 +49,8 @@ const App = () => {
 }
 
 export default App
+// Also removes onSubmit={addNote} from the <div/> around the input form.
+
 // The examples on the course page has value={newNote} in the <input> component.
 // However, it seems to work fine without it. I am not sure what that does.
   //const newNote = (event) => {
--- part2/phoneBook/src/Numbers.jsx
+++ part2/phoneBook/src/Numbers.jsx
@@ -4,7 +4,7 @@ const Numbers = (props) => {
       <div>
         <h2>Numbers</h2>
         {props.persons.map(name => 
-          <p key={name.name}>{name.name}</p> 
+          <p key={name.name}>{name.name} {name.number}</p> 
       )}
       </div>
     )
