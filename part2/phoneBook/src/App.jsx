32c35b10e31006634bd8d4f5b5bf0dfdbda08a31
roong0 <altwd@proton.me>
2023-12-20 12:14:55 +1100

    Q2.11

--- part2/phoneBook/src/App.jsx
+++ part2/phoneBook/src/App.jsx
@@ -1,15 +1,11 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Numbers from './Numbers'
 import Phonebook from './Phonebook';
 import Form from './Form';
+import axios from 'axios';
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [newPerson, setNewPerson] = useState('');
@@ -44,6 +40,17 @@ const App = () => {
     peeps = peeps.filter((x)=>x.name.toLowerCase().includes(newPerson.toLowerCase()));
     setVisitblePersons(peeps);
   }
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled');
+        setPersons(response.data);
+        //Assumes all visible for first render(Cant set it to persons as it renders only does this some time in the future)
+        setVisitblePersons(response.data);
+      })
+  }, [])
 
   return (
     <div>
